1a5bd7448482f30411aafce68e70354825a40b80
gg161087 <gg161087@gmail.com>
2023-11-29 22:12:29 -0300

    [Clase 28] CRUD de post completo

--- Codigo/CaCFullStack/backend/index.js
+++ Codigo/CaCFullStack/backend/index.js
@@ -20,7 +20,6 @@ const conn = async () => {
     }
 }
 
-
 app.listen(port, () => {
     conn()
     console.log(`servidor corriendo http://localhost:${port}`);
--- Codigo/CaCFullStack/backend/routes/postRoutes.js
+++ Codigo/CaCFullStack/backend/routes/postRoutes.js
@@ -5,4 +5,7 @@ import postController from './../controllers/postController.js';
 export const postRoutes = Router()
 
 postRoutes.get('/', postController.getAllPosts);
-postRoutes.get('/:id', postController.getPostById);
+postRoutes.get('/:id', postController.getPost);
+postRoutes.post('/', postController.createPost);
+postRoutes.put('/:id', postController.editPost);
+postRoutes.delete('/:id', postController.deletePost);
